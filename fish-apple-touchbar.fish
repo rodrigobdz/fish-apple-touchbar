d5a44bff3a050599a525480405ac7884d6f6ec59
Scott Miller <scott@millergeek.xyz>
2019-03-20 09:28:52 -0400

    use guard clause

--- fish-apple-touchbar.fish
+++ fish-apple-touchbar.fish
@@ -1,31 +1,32 @@
 function fish-apple-touchbar
     if status is-interactive
-        function __fish_apple_touchbar_first_view
-            function bind_keys_function
-                __fish_apple_touchbar_bind_key 1 '👉 pwd' "pwd" '-s'
-                __fish_apple_touchbar_bind_key 2 'second view' '__fish_apple_touchbar_second_view'
-                __fish_apple_touchbar_bind_key 3 'third view' '__fish_apple_touchbar_third_view'
-            end
-
-            __fish_apple_touchbar_create_view 'first' bind_keys_function
+        exit 0
+    end
+    function __fish_apple_touchbar_first_view
+        function bind_keys_function
+            __fish_apple_touchbar_bind_key 1 '👉 pwd' "pwd" '-s'
+            __fish_apple_touchbar_bind_key 2 'second view' '__fish_apple_touchbar_second_view'
+            __fish_apple_touchbar_bind_key 3 'third view' '__fish_apple_touchbar_third_view'
         end
 
-        function __fish_apple_touchbar_second_view
-            function bind_keys_function
-                __fish_apple_touchbar_bind_key 1 '👈 back' '__fish_apple_touchbar_first_view'
-                __fish_apple_touchbar_bind_key 2 'current path' "pwd" '-s'
-            end
+        __fish_apple_touchbar_create_view 'first' bind_keys_function
+    end
 
-            __fish_apple_touchbar_create_view 'second' bind_keys_function
+    function __fish_apple_touchbar_second_view
+        function bind_keys_function
+            __fish_apple_touchbar_bind_key 1 '👈 back' '__fish_apple_touchbar_first_view'
+            __fish_apple_touchbar_bind_key 2 'current path' "pwd" '-s'
         end
 
-        function __fish_apple_touchbar_third_view
-            function bind_keys_function
-                __fish_apple_touchbar_bind_key 1 '👈 back' '__fish_apple_touchbar_first_view'
-                __fish_apple_touchbar_bind_key 2 'ls' "ls -la" '-s'
-            end
+        __fish_apple_touchbar_create_view 'second' bind_keys_function
+    end
 
-            __fish_apple_touchbar_create_view 'third' bind_keys_function
+    function __fish_apple_touchbar_third_view
+        function bind_keys_function
+            __fish_apple_touchbar_bind_key 1 '👈 back' '__fish_apple_touchbar_first_view'
+            __fish_apple_touchbar_bind_key 2 'ls' "ls -la" '-s'
         end
+
+        __fish_apple_touchbar_create_view 'third' bind_keys_function
     end
 end
