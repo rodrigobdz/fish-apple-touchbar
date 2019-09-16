67e7a8f933e8a92ac621dd9ec5ca5728d5b370e2
Ramon Rüttimann <tommyknows@users.noreply.github.com>
2019-09-16 10:57:47 -0700

    Check and use vi-mode / bindings (#5)
    * check for vim mode
    
    * avoid repetition of `/dev/null`
    
    Co-Authored-By: Rodrigo Bermúdez Schettino <rodrigobdz@users.noreply.github.com>

--- functions/__fish_apple_touchbar_bind_key.fish
+++ functions/__fish_apple_touchbar_bind_key.fish
@@ -1,8 +1,12 @@
 function __fish_apple_touchbar_bind_key --argument-names fn_number fn_text fn_command bind_option
     __fish_apple_touchbar_print_key "SetKeyLabel=F$fn_number=$fn_text"
 
+    if bind -M insert >/dev/null 2>&1
+        set vimbind "-M" "insert"
+    end
+
     if [ "$bind_option" = "-s" ]
-        bind --key f$fn_number "commandline -r '$fn_command'; commandline -f execute"
+        bind $vimbind --key f$fn_number "commandline -r '$fn_command'; commandline -f execute"
     else
         bind --key f$fn_number $fn_command
     end
