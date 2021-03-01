3a4dc558483329c351d0002f5500787522def5ae
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-03-01 12:08:00 +0100

    Move init.fish to conf.d/plugin_name.fish
    Follow fisher's recommendation https://github.com/jorgebucaran/fisher/issues/651#issuecomment-762808325.

--- conf.d/fish_apple_touchbar.fish
+++ conf.d/fish_apple_touchbar.fish
@@ -1,3 +1,7 @@
+# Moved contents of init.fish here
+# Source: https://github.com/jorgebucaran/fisher/issues/651#issuecomment-762808325
+fish-apple-touchbar
+
 set -l name (basename (status -f) .fish){_uninstall}
 
 function __fish_apple_touchbar_uninstall --on-event $name
