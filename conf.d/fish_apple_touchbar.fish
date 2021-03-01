a5b0205ef62a9a72b24dd1f75cb748b7a8a6efd5
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-03-01 12:27:38 +0100

    Move functions/keybindings.fish to conf.d/fish_apple_touchbar.fish
    Quote variables to fix fish linter error about missing argument for --on-event.
    Follow fisher's recommended filename for file https://github.com/jorgebucaran/fisher/tree/56c629a3a9086f33eeae801598b2046cab9bea6b#event-system.

--- conf.d/fish_apple_touchbar.fish
+++ conf.d/fish_apple_touchbar.fish
@@ -2,9 +2,17 @@
 # Source: https://github.com/jorgebucaran/fisher/issues/651#issuecomment-762808325
 fish-apple-touchbar
 
-set --local name (basename (status --current-filename) .fish){_uninstall}
+# Expected value should be fish_apple_touchbar_install
+set --local plugin_name_install (basename (status --current-filename) .fish){_install}
 
-function __fish_apple_touchbar_uninstall --on-event $name
+# Expected value should be fish_apple_touchbar_uninstall
+set --local plugin_name_uninstall (basename (status --current-filename) .fish){_uninstall}
+
+function __fish_apple_touchbar_uninstall --on-event "$plugin_name_install"
+    __fish_apple_touchbar_first_view
+end
+
+function __fish_apple_touchbar_uninstall --on-event "$plugin_name_uninstall"
     __fish_apple_touchbar_reset_bindings
     functions --erase (functions --all | command awk '/^__fish_apple_touchbar/')
 end
