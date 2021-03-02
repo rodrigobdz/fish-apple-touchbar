13b9f3c3c9575634cc8d862d582aade1306c6fde
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-03-02 13:13:07 +0100

    Conf.d: Fix install and uninstall events
    Fixed typo in install function name.
    Removed curly braces from variables, their values were including these—e.g. fish_apple_touchbar{_install}.

--- conf.d/fish_apple_touchbar.fish
+++ conf.d/fish_apple_touchbar.fish
@@ -3,12 +3,12 @@
 fish-apple-touchbar
 
 # Expected value should be fish_apple_touchbar_install
-set --local plugin_name_install (basename (status --current-filename) .fish){_install}
+set --local plugin_name_install (basename (status --current-filename) .fish)_install
 
 # Expected value should be fish_apple_touchbar_uninstall
-set --local plugin_name_uninstall (basename (status --current-filename) .fish){_uninstall}
+set --local plugin_name_uninstall (basename (status --current-filename) .fish)_uninstall
 
-function __fish_apple_touchbar_uninstall --on-event "$plugin_name_install"
+function __fish_apple_touchbar_install --on-event "$plugin_name_install"
     __fish_apple_touchbar_first_view
 end
 
