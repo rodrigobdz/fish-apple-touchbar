003f87157e9bb4a4d754b87d828436f0b425d4c6
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-03-02 17:11:41 +0100

    Functions: Erase also --mode insert bindings

--- functions/__fish_apple_touchbar_reset_bindings.fish
+++ functions/__fish_apple_touchbar_reset_bindings.fish
@@ -2,6 +2,7 @@ function __fish_apple_touchbar_reset_bindings
     function __fish_apple_touchbar_unbind_keys
         for n in (seq 12)
             bind --erase --key f$n
+            bind --erase --mode insert --key f$n
         end
     end
 
