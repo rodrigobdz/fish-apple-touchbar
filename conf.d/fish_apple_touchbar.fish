bf6636c14b7fdca1cedddcfbaaa985fd363493ff
Kid <44045911+kidonng@users.noreply.github.com>
2021-07-06 16:49:55 +0800

    Remove unnecessary `basename` and `awk` call (#15)
    * Remove unnecessary basename and awk call
    
    * Replace `exit` with `return`

--- conf.d/fish_apple_touchbar.fish
+++ conf.d/fish_apple_touchbar.fish
@@ -2,17 +2,11 @@
 # Source: https://github.com/jorgebucaran/fisher/issues/651#issuecomment-762808325
 fish-apple-touchbar
 
-# Expected value should be fish_apple_touchbar_install
-set --local plugin_name_install (basename (status --current-filename) .fish)_install
-
-# Expected value should be fish_apple_touchbar_uninstall
-set --local plugin_name_uninstall (basename (status --current-filename) .fish)_uninstall
-
-function __fish_apple_touchbar_install --on-event "$plugin_name_install"
+function __fish_apple_touchbar_install --on-event "fish_apple_touchbar_install"
     __fish_apple_touchbar_first_view
 end
 
-function __fish_apple_touchbar_uninstall --on-event "$plugin_name_uninstall"
+function __fish_apple_touchbar_uninstall --on-event "fish_apple_touchbar_uninstall"
     __fish_apple_touchbar_reset_bindings
-    functions --erase (functions --all | command awk '/^__fish_apple_touchbar/')
+    functions --erase (functions --all | string match --regex --entire '^__fish_apple_touchbar')
 end
